9179bef005fab37c2dc93cde6a0c57f98521047f
Léo BIREBENT <LBIREBENT@ocelot.cloud>
2022-04-14 17:24:09 +0200

    premier script symfo!

---
+++ src/Controller/ControleurAcc.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ControleurAcc extends AbstractController
+{
+
+	#[Route('/', name: 'accueil')]
+	public function index(Request $request): Response
+	{
+		/*return $this->render('conference/index.html.twig',
+				['controller_name'=>'Controleur Accueil']);
+		*/
+		$greet = '';
+		if ($name = $request->query->get('hello'))
+		{
+			$greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+		}
+
+		return new Response(<<<EOF
+<html>
+<head>
+<title>Accueil</title>
+</head>
+<body>
+SALUT
+$greet
+<img src="/images/under-construction.gif" />
+</body>
+</html>
+EOF
+		);
+	}
+}
+?>
